179356206bd628d84a8b39b5410edcf336791466
Jeff42 <jeanfrancois.rebaud@yahoo.fr>
2017-02-17 22:53:32 +0100

    Premier test envoie requete get sur searchsalles pour new salle orga

--- restServeur.js
+++ restServeur.js
@@ -1,11 +1,22 @@
-var express = require('express')
-var mongodb = require('mongodb')
+var express = require('express');
+var mongodb = require('mongodb');
 
-var app = express()
-var MongoClient = mongodb.MongoClient
+var app = express();
+var MongoClient = mongodb.MongoClient;
 
-var url = 'mongodb://localhost:27017/lopitadminprep'
+var url = 'mongodb://localhost:27017/lopitadminprep';
 MongoClient.connect(url, function (err, db){
-	if (err) throw err
-	db.collection ()
-})
+	if (err) throw err;
+
+	db.collection ('SearchSalles', function(err, collection){
+
+		app.get('/localpitsymf/orga/newsalle/searchsalles', function(req, res){
+			collection.find().toArray(function(err, salles){
+                if (err) throw err;
+                res.send(salles);
+			})
+		})
+	})
+});
+
+var server = app.listen(8080);
